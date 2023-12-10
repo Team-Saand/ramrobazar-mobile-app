2725a5bba912b80762a916782d56132a8a65c127
Nishant Sapkota <snishant306@gmail.com>
2023-12-10 19:29:06 +0545

    chore: change failure to represent actual api failure

--- lib/core/failure/failure.dart
+++ lib/core/failure/failure.dart
@@ -1,9 +1,44 @@
 class Failure {
-  final String error;
-  final String? statusCode;
+  bool success;
+  String timestamp;
+  String path;
+  dynamic error;
 
   Failure({
+    required this.success,
+    required this.timestamp,
+    required this.path,
     required this.error,
-    this.statusCode,
   });
+
+  factory Failure.fromJson(Map<String, dynamic> json) {
+    return Failure(
+      success: json['success'] ?? false,
+      timestamp: json['timestamp'] ?? '',
+      path: json['path'] ?? '',
+      error: json['error'] is Map
+          ? ErrorDetails.fromJson(json['error'])
+          : json['error'],
+    );
+  }
+}
+
+class ErrorDetails {
+  String message;
+  String error;
+  int statusCode;
+
+  ErrorDetails({
+    required this.message,
+    required this.error,
+    required this.statusCode,
+  });
+
+  factory ErrorDetails.fromJson(Map<String, dynamic> json) {
+    return ErrorDetails(
+      message: json['message'] ?? '',
+      error: json['error'] ?? '',
+      statusCode: json['statusCode'] ?? 0,
+    );
+  }
 }
